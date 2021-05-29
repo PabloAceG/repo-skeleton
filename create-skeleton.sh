283fe785d892008fdda7806ea39a5c6292782b67
PabloAceG <pabloacereda98@gmail.com>
2021-05-29 19:31:56 +0200

    Config file and License
    - Optionally use configuration file instead of arguments.
    - License structure and link.

--- create-skeleton.sh
+++ create-skeleton.sh
@@ -12,11 +12,16 @@ set -euo pipefail
 
 # Defaults
 REPO_NAME=""
-OWNER=""
+REPO_OWNER=""
+LICENSE_NAME=""
 TECHNOLOGIES=[]
 DEPENDABOT=false    # Don't activate Dependabot.
 FORCE_PR=false      # Don't for PRs for contributions.
 PUSH=true           # Push to remote repository.
+FILE=""
+
+# Urls
+LICENSE_URL="https://github.com/licenses/license-templates/tree/master/templates/"
 
 function help() {
   echo "NAME"
@@ -31,16 +36,20 @@ function help() {
   echo "    -o, --owner <repository-owner>"
   echo "        Mandatory (only if pushing to remote is active). GitHub owner of the repository."
   echo "        NOTE: Must have permissions to create the repository for this owner."
+  echo "    -l, --license <license-name>"
+  echo "        Optional. Select license for the repository."
+  echo "        NOTE: Look at the following repository to choose the best option: "
+  echo "        $LICENSE_URL"
   echo "    -d, --activate-dependabot"
-  echo "        Activate Dependabot dependency analysis."
+  echo "        Optional. Activate Dependabot dependency analysis."
   echo "    -p, --force-pr-contribution"
-  echo "        For using PRs for code contributions, no code can be pushed to master branch."
+  echo "        Optional. For using PRs for code contributions, no code can be pushed to master branch."
   echo "    -r, --no-push-remote"
-  echo "        Push created skeleton to remote repository."
+  echo "        Optional. Push created skeleton to remote repository."
   echo "        NOTE: It only creates the repository. If the repository is already created, no changes are staged."
   echo "        NOTE: Pushes to remote by defualt."
   echo "    -f, --file"
-  echo "        File configuration of this script."
+  echo "        Optional. File configuration of this script."
   echo "EXAMPLES"
   echo "    create-skeleton.sh --help"
   echo "        To better know how this script works."
@@ -70,7 +79,14 @@ function get_parameters() {
         ;;
       --owner | -o)
         shift
-        OWNER=$1
+        REPO_OWNER=$1
+      ;;
+      --license | -l)
+        shift
+        LICENSE_NAME=$1
+      ;;
+      --techs | -t)
+        echo "On the making"
       ;;
       --activate-dependabot | -d)
         DEPENDABOT=true
@@ -82,9 +98,8 @@ function get_parameters() {
         PUSH=false
       ;;
       --file | -f)
-          echo "Using a file for configuration is still on the making."
-          echo "Run --help command to see how to configure the options manually."
-          exit 0
+          shift
+          FILE=$1
       ;;
       --help | -h)
         help
@@ -98,17 +113,48 @@ function get_parameters() {
   done
 
   # Check for mandatory fields
-  [ -z "$REPO_NAME" ] && echo "[ERROR]: --name option is mandatory. See --help for more information."
-  "$PUSH" && [ -z "$OWNER" ] && echo "[ERROR]: The skeleton is going to be pushed to a remote repository. Needs an --owner to continue."
-  [ ! -z "$OWNER" ] && ! "$PUSH" && echo "[WARNING]: The repository skeleton won't be pushed to remote repository. --owner will be ignored."
+  if [ -z "$FILE" ]
+  then
+    [ -z "$REPO_NAME" ] && echo "[ERROR]: --name option is mandatory. See --help for more information."
+    "$PUSH" && [ -z "$REPO_OWNER" ] && echo "[ERROR]: The skeleton is going to be pushed to a remote repository. Needs an --owner to continue."
+    [ ! -z "$REPO_OWNER" ] && ! "$PUSH" && echo "[WARNING]: The repository skeleton won't be pushed to remote repository. --owner will be ignored."
+  else
+      [ ! -z "$REPO_NAME" ] && echo "[WARNING]: Used configuration file instead. --name/-n will be ignored."
+      [ ! -z "$REPO_OWNER" ] && echo "[WARNING]: Used configuration file instead. --owner/-o will be ignored."
+      [ ! -z "$LICENSE_NAME" ] && echo "[WARNING]: Used configuration file instead. --license/-l will be ignored."
+      [ ! -z "$TECHNOLOGIES" ] && echo "[WARNING]: Used configuration file instead. --techs/-t will be ignored."
+      [ ! -z "$DEPENDABOT" ] && echo "[WARNING]: Used configuration file instead. --activate-dependabot/-d will be ignored."
+      [ ! -z "$FORCE_PR" ] && echo "[WARNING]: Used configuration file instead. --force-pr-contribution/-p will be ignored."
+      [ ! -z "$PUSH" ] && echo "[WARNING]: Used configuration file instead. --no-push-remote/-r will be ignored."
+      # Check for file existance
+      if [ ! -e "$FILE" ]
+      then
+        echo "[ERROR]: The specified configuration file does not exist. Check if the path is correct."
+        exit 1
+      fi
+      # Get parameters from configuration file
+      source "$FILE"
+      REPO_NAME="$REPO"
+      REPO_OWNER="$OWNER"
+      LICENSE_NAME="$LICENSE"
+      TECHNOLOGIES="$TECHS"
+      FORCE_PR="$PR"
+      PUSH="$REMOTE"
+  fi
 }
 
 function check_dependencies() {
   if [[ ! $(which git) ]]
   then
-    echo "Git is a dependency for this script. Install it before continuing."
+    echo "[ERROR]: git is a dependency for this script. Install it before continuing."
     exit 1
   fi
+
+  if [[ ! $(which curl) ]]
+  then
+    echo "[ERROR]: curl is a dependency for this script. Install it before continuing."
+  fi
+
   # TODO: check technologies dependencies
 }
 
@@ -117,6 +163,15 @@ function create_repo() {
     get_parameters "$@"
     # See if all dependencies are installed
     check_dependencies
+
+    # BUG: Repo might exist
+    # Create initial commit
+    #mkdir "$REPO_NAME"
+    #cd "$REPO_NAME"
+    #echo "$REPO_NAME" > README.md
+    #git init
+    #git add README.md
+    #git commit -m "First commit"
 }
 
 create_repo "$@"
