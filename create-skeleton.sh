2275e4ec5b2327f608b66901a751bb61c1d5d968
PabloAceG <pabloacereda98@gmail.com>
2021-06-07 22:36:38 +0200

    Reformat configuration file.
    - Reformat configuration file including sections and comments per field.
    - Small checks for fields.

--- create-skeleton.sh
+++ create-skeleton.sh
@@ -224,6 +224,12 @@ function get_parameters() {
   [ -z "$REPO_NAME" ] && echo "[ERROR]: --name option is mandatory. See --help for more information."
   [ -z "$REPO_OWNER" ] && echo "[ERROR]: --owner argument is mandatory. See --help for more information."
   [ -z "$ACCESS_TOKEN" ] && "$PUSH" && echo "[ERROR]: --token argument is mandatory. See --help for more information."
+  [ -z "$PUSH" ] && PUSH=false && echo "[WARNING]: Not pushing repository, as none was specified."
+  if [[ -z "$PRIVATE" ]]
+  then
+    PRIVATE=true
+    "$PUSH" && "[WARNING]: Making repository private, as none was specified."
+  fi
 
   echo "[INFO]: Parameters read."
 }
